e603a40fcc67fcd2aeb2034d7b987db8dab0ad39
uri <uriellua14@gmail.com>
2021-05-09 16:32:05 +0000

    remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sic6lfq)=xtyg%qdek)38ajc@bomc$5lgowv_%s0ty3%(d-t7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['urlellua95.pythonanywhere.com']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'portfolio',
-    
+
 ]
 
 MIDDLEWARE = [
